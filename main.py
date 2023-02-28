45ee978b297158a33b000e04651ad61ca8179fc2
Edgarbanzo <edgarzepedau@hotmail.com>
2023-02-28 12:16:36 -0600

    Init Project

---
+++ main.py
@@ -0,0 +1,14 @@
+from PySide6.QtWidgets import QApplication
+
+from controllers.main_window import MainForm 
+import sys
+
+if __name__ == '__main__':
+    # Aplicación de Qt
+    app = QApplication()
+    # Se crea un botón con la palabra Hola
+    window=MainForm()
+    # Se hace visible el botón
+    window.show()
+    # Qt loop
+    sys.exit(app.exec()) #use exec?
